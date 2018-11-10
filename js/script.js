908ffa190c2dd46e6985fd0caa1e1978645c02c7
mateuszmagier <mateuszmagier@bitbucket.org>
2018-11-10 22:33:22 +0100

    Initial commit.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(function () {
+    let categories = $('.category');
+    let activeCategory = $('.category--active');
+    let allProjects = $('.project');
+    let websitesProjects = $('.project[data-category="websites"]');
+    let gamesProjects = $('.project[data-category="games"]');
+    let webAppsProjects = $('.project[data-category="web-apps"]');
+
+    categories.click(function() {
+        activeCategory.removeClass('category--active');
+        activeCategory = $(this); // change active category
+        $(this).addClass('category--active');
+        if(activeCategory.hasClass('category--all')) { // 'all' category
+            allProjects.addClass('visible');
+        } else { // other category
+            allProjects.removeClass('visible');
+            if(activeCategory.hasClass('category--websites')) {
+                websitesProjects.addClass('visible');
+            } else if(activeCategory.hasClass('category--games')) {
+                gamesProjects.addClass('visible');
+            } else if(activeCategory.hasClass('category--web-apps')) {
+                webAppsProjects.addClass('visible');
+            }
+        }
+    });
+
+});
