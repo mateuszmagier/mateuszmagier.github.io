503957ddeee4579b3c409bd1431d96d1368c1f57
mateuszmagier <mateuszmagier@bitbucket.org>
2018-11-12 14:35:16 +0100

    Modify home section and add footer.

--- js/script.js
+++ js/script.js
@@ -6,30 +6,28 @@ $(function () {
     let gamesProjects = $('.project[data-category="games"]');
     let webAppsProjects = $('.project[data-category="web-apps"]');
 
-    categories.click(function() {
+    categories.click(function () {
         activeCategory.removeClass('category--active');
         activeCategory = $(this); // change active category
         $(this).addClass('category--active');
-        if(activeCategory.hasClass('category--all')) { // 'all' category
+        if (activeCategory.hasClass('category--all')) { // 'all' category
             allProjects.addClass('visible');
         } else { // other category
             allProjects.removeClass('visible');
-            if(activeCategory.hasClass('category--websites')) {
+            if (activeCategory.hasClass('category--websites')) {
                 websitesProjects.addClass('visible');
-            } else if(activeCategory.hasClass('category--games')) {
+            } else if (activeCategory.hasClass('category--games')) {
                 gamesProjects.addClass('visible');
-            } else if(activeCategory.hasClass('category--web-apps')) {
+            } else if (activeCategory.hasClass('category--web-apps')) {
                 webAppsProjects.addClass('visible');
             }
         }
     });
 
-    $('.skills-section__skill').mouseenter(function() {
-        console.log('mouseenter');
+    $('.skills-section__skill').mouseenter(function () {
         $(this).children('i').toggleClass('colored');
     });
-    $('.skills-section__skill').mouseleave(function() {
-        console.log('mouseleave');
+    $('.skills-section__skill').mouseleave(function () {
         $(this).children('i').toggleClass('colored');
     });
 
